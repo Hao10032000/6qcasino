677e384d3b8ab874df34dfbd97145bcd7cc5d814
hao <themesflatc10@gmail.com>
2023-12-19 16:11:24 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+/**
+    * headerFixed
+*/
+
+; (function ($) {
+
+  "use strict";
+
+  var selectImages = function () {
+    if ($(".image-select").length > 0) {
+      const selectIMG = $(".image-select");
+      selectIMG.find("option").each((idx, elem) => {
+        const selectOption = $(elem);
+        const imgURL = selectOption.attr("data-thumbnail");
+        if (imgURL) {
+          selectOption.attr(
+            "data-content",
+            "<img src='%i'/> %s"
+              .replace(/%i/, imgURL)
+              .replace(/%s/, selectOption.text())
+          );
+        }
+      });
+      selectIMG.selectpicker();
+    }
+  };
+
+  var menuleft = function () {
+    if ($('div').hasClass('section-menu-left')) {
+
+      $(document).on("click", ".menu-item.has-children", function () {
+        var args = { duration: 600 };
+        if ($(this).hasClass("active")) {
+          $(this).children(".sub-menu").slideUp(args);
+          $(this).removeClass("active");
+        } else {
+          $(".sub-menu").slideUp(args);
+          $(this).children(".sub-menu").slideDown(args);
+          $(".menu-item.has-children").removeClass("active");
+          $(this).addClass("active");
+        }
+    });
+    }
+  };
+
+  var tabs = function(){
+    $('.widget-tabs').each(function(){
+        $(this).find('.widget-content-tab').children().hide();
+        $(this).find('.widget-content-tab').children(".active").show();
+        $(this).find('.widget-menu-tab').find('li').on('click',function(){
+            var liActive = $(this).index();
+            var contentActive=$(this).siblings().removeClass('active').parents('.widget-tabs').find('.widget-content-tab').children().eq(liActive);
+            contentActive.addClass('active').fadeIn("slow");
+            contentActive.siblings().removeClass('active');
+            $(this).addClass('active').parents('.widget-tabs').find('.widget-content-tab').children().eq(liActive).siblings().hide();
+        });
+    });
+  };
+
+  
+
+
+  // Dom Ready
+  $(function () {
+    selectImages();
+    menuleft();
+    tabs();
+  });
+
+})(jQuery);
