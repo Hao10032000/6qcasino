0c9380ad35e2388b1773ec4f53ac438d3493cd61
hao <themesflatc10@gmail.com>
2023-12-20 16:59:52 +0700

    20/12

--- js/swiper.js
+++ js/swiper.js
@@ -26,3 +26,38 @@ var sliderinit = function() {
     }
 }
 sliderinit();
+
+var swiper =  new Swiper(".carousel-5", {
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+    slidesPerView: 2,   
+    loop: true,
+    spaceBetween: 30,
+    speed: 10000,
+    observer: true,
+    observeParents: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+        868: {
+            slidesPerView: 6,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 7,
+            spaceBetween: 30,
+        },
+    },
+});
